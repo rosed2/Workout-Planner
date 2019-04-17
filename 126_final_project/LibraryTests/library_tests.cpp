615ac41f3a61aa8177bca9f7c7f7d5fe100ad30e
Rose Dinh <rosed2@illinois.edu>
2019-04-17 01:02:07 -0500

    Add more library test cases

--- 126_final_project/LibraryTests/library_tests.cpp
+++ 126_final_project/LibraryTests/library_tests.cpp
@@ -19,7 +19,7 @@ using std::string;
 
 //Setup
 Exercise ex1 = Exercise("Exercise 1", "bicep", "none");
-Exercise ex2 = Exercise("Exercise 2", "quads", "dumbbells");
+Exercise ex2 = Exercise("Exercise 2", "bicep", "dumbbells");
 Exercise ex3 = Exercise("Exercise 3", "back", "none");
 
 vector<Exercise> all_exercises{ ex1, ex2, ex3 };
@@ -31,6 +31,7 @@ vector<Exercise> vec3{ ex3 };
 WorkoutPlan w1 = WorkoutPlan("Plan 1", vec1);
 WorkoutPlan w2 = WorkoutPlan("Plan 2", vec2);
 WorkoutPlan w3 = WorkoutPlan("Plan 3", vec3);
+
 
 //---------------------Tests for GetWorkoutPlans----------
 TEST_CASE("Library GetWorkoutPlans") {
@@ -47,6 +48,8 @@ TEST_CASE("Library GetWorkoutPlans Empty") {
 	REQUIRE(library.GetWorkoutPlans().size() == 0);
 }
 
+
+
 //--------------------Tests for AddWorkoutPlan---------------
 TEST_CASE("Library AddWorkoutPlan") {
 	vector<WorkoutPlan> work{ w1 };
@@ -59,6 +62,8 @@ TEST_CASE("Library AddWorkoutPlan") {
 	REQUIRE(library.GetWorkoutPlans().size() == 2);
 	REQUIRE(library.GetWorkoutPlans()[1].GetName() == "Plan 2");
 }
+
+
 
 //--------------------Tests for SearchForPlanByName--------
 TEST_CASE("Library SearchForPlanByName Test Case Insensitive") {
@@ -91,6 +96,8 @@ TEST_CASE("Library SearchForPlanByName No Results") {
 	REQUIRE(results.size() == 0);
 }
 
+
+
 //------------------Tests for SearchForPlanByExercise------------
 TEST_CASE("Library SearchForPlanByExercise No Results") {
 	vector<WorkoutPlan> work{ w1, w2 };
@@ -118,4 +125,112 @@ TEST_CASE("Library SearchForPlanByExercise One Result") {
 	vector<WorkoutPlan> results = library.SearchForPlanByExercise("3");
 	REQUIRE(results.size() == 1);
 	REQUIRE(results[0].GetName() == "Plan 3");
+}
+
+
+//------------------Test for GetAllExercises----------
+TEST_CASE("GetAllExercises") {
+	vector<WorkoutPlan> work{ w1, w3 };
+	Library library = Library(work, all_exercises);
+	REQUIRE(library.GetAllExercises().size() == 3);
+}
+
+
+
+//---------------Test for SearchForExercisesByName-------------
+TEST_CASE("Library SearchForExerciseByName Test Case Insensitive") {
+	vector<WorkoutPlan> work{ w1, w2 };
+
+	Library library = Library(work, all_exercises);
+
+	vector<Exercise> results = library.SearchForExercisesByName("eXErCi");
+	REQUIRE(results.size() == 3);
+	REQUIRE(results[0].GetName() == "Exercise 1");
+	REQUIRE(results[1].GetName() == "Exercise 2");
+	REQUIRE(results[2].GetName() == "Exercise 3");
+}
+
+TEST_CASE("Library SearchForExerciseByName No Results") {
+	vector<WorkoutPlan> work{ w1, w2 };
+
+	Library library = Library(work, all_exercises);
+
+	vector<Exercise> results = library.SearchForExercisesByName("zbs");
+	REQUIRE(results.size() == 0);
+}
+
+TEST_CASE("Library SearchForExerciseByName Number") {
+	vector<WorkoutPlan> work{ w1, w2 };
+
+	Library library = Library(work, all_exercises);
+
+	vector<Exercise> results = library.SearchForExercisesByName("1");
+	REQUIRE(results.size() == 1);
+	REQUIRE(results[0].GetName() == "Exercise 1");
+}
+
+
+
+//---------------Test for SearchForExercisesByMuscle-------------
+TEST_CASE("Library SearchForExerciseByMuscle Test Case Insensitive") {
+	vector<WorkoutPlan> work{ w1, w2 };
+
+	Library library = Library(work, all_exercises);
+
+	vector<Exercise> results = library.SearchForExercisesByMuscle("bAck");
+	REQUIRE(results.size() == 1);
+	REQUIRE(results[0].GetName() == "Exercise 3");
+}
+
+TEST_CASE("Library SearchForExerciseByMuscle No Results") {
+	vector<WorkoutPlan> work{ w1, w2 };
+
+	Library library = Library(work, all_exercises);
+
+	vector<Exercise> results = library.SearchForExercisesByMuscle("zbs");
+	REQUIRE(results.size() == 0);
+}
+
+TEST_CASE("Library SearchForExerciseByMuscle 2 Results") {
+	vector<WorkoutPlan> work{ w1, w2 };
+
+	Library library = Library(work, all_exercises);
+
+	vector<Exercise> results = library.SearchForExercisesByMuscle("biCeP");
+	REQUIRE(results.size() == 2);
+	REQUIRE(results[0].GetName() == "Exercise 1");
+	REQUIRE(results[1].GetName() == "Exercise 2");
+}
+
+
+
+//---------------Test for SearchForExercisesByEquipment-------------
+TEST_CASE("Library SearchForExerciseByEquipment Test Case Insensitive") {
+	vector<WorkoutPlan> work{ w1, w2 };
+
+	Library library = Library(work, all_exercises);
+
+	vector<Exercise> results = library.SearchForExercisesByEquipment("dumBBell");
+	REQUIRE(results.size() == 1);
+	REQUIRE(results[0].GetName() == "Exercise 2");
+}
+
+TEST_CASE("Library SearchForExerciseByEquipment No Results") {
+	vector<WorkoutPlan> work{ w1, w2 };
+
+	Library library = Library(work, all_exercises);
+
+	vector<Exercise> results = library.SearchForExercisesByEquipment("cable");
+	REQUIRE(results.size() == 0);
+}
+
+TEST_CASE("Library SearchForExerciseByEquipment 2 Results") {
+	vector<WorkoutPlan> work{ w1, w2 };
+
+	Library library = Library(work, all_exercises);
+
+	vector<Exercise> results = library.SearchForExercisesByEquipment("noNe");
+	REQUIRE(results.size() == 2);
+	REQUIRE(results[0].GetName() == "Exercise 1");
+	REQUIRE(results[1].GetName() == "Exercise 3");
 }
